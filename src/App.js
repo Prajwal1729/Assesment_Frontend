cbea5ec2179093e5a96b69a60a732e97c96618b9
Prajwal Chavan <prajwalchavan@Prajwals-MacBook-Air.local>
2026-04-25 22:42:07 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import CustomerDetails from './pages/CustomerDetails';
 import Transactions from './pages/Transactions';
 
+
 function App() {
   return (
     <div className="App">
